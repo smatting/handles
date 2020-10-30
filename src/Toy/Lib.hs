4b1be3beaaffd995789fe69f45ec1e37ff9544b9
Stefan Matting <stefan@wire.com>
2020-10-30 11:17:25 +0100

    more comments

--- src/Toy/Lib.hs
+++ src/Toy/Lib.hs
@@ -6,6 +6,7 @@ import qualified Toy.Logger as Logger
 import qualified Toy.UserService as UserService
 import qualified Toy.UserService.Remote as Remote
 
+-- if you squint hard enough this almost looks like MTL or an effect system ;)
 program ::
   ( Has UserService.Handle e,
     Has Logger.Handle e
@@ -16,7 +17,11 @@ program = do
   Logger.log ("User created: " <> show user)
 
 -- Here we "interpret" the program by providing the handles.
--- Thanks to `data-has` the environment can be just an adhoc tuple. No boilerplate here :)
+--
+-- Thanks to `data-has` the environment can be just an adhoc tuple containing
+-- implementations of all the handles (in any order).
+-- Without `daha-has` you need to define an Env type and write HasLogger, HasUserService
+-- that describe how to pick the handle from env.
 main = do
   let logger = Logger.new
   let config = Remote.Config {baseUrl = "http://user-service:9090"}
